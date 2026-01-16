95965516ce3fe11a55da17fedf96865a636660d9
Your Name <vivekchavan942@gmail.com>
2026-01-16 20:52:01 +0530

    Some bug

--- Frontend/src/pages/MyPosts.jsx
+++ Frontend/src/pages/MyPosts.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux"
-import RecipeCard from "../components/recipeCard"
+import RecipeCard from "../components/RecipeCard"
 import UserContext from "../contexts/userContext"
 import { useNavigate } from "react-router-dom"
 import { useContext } from "react"
